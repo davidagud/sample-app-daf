07f9fc2346c3bef3e0f0e7b907109a3c6b15c17e
David Agudelo <davidagud@gmail.com>
2018-09-08 22:29:25 -0500

    5.2 added product search

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
-  root 'products#index'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
