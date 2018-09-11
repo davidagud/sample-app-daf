8c3a11bc910da667867252330bfede3860646889
David Agudelo <davidagud@gmail.com>
2018-09-10 21:53:24 -0500

    5.3 added mailer

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: '587',
   authentication: :plain,
-  user_name: apikey,
-  password: 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,
+  user_name: 'apikey',
+  password: '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',
   domain: 'heroku.com',
   enable_starttls_auto: true
 }
