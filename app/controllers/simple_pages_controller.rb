e795ab96c7b22f10ba4be9a025afb26bf5d18242
David Agudelo <davidagud@gmail.com>
2018-09-10 20:58:54 -0500

    5.3 added mailer

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -8,3 +8,14 @@ class SimplePagesController < ApplicationController
     @products = Product.limit(4)
   end
 end
+
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(
+    from: @email,
+    to: 'davidagud@gmail.com',
+    subject: "A new contact form from #{@name}",
+    body: @message).deliver_now
+end
