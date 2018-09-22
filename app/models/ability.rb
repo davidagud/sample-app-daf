c40505bc3c68cdcc4b64baca35dc40e7ff0f72ed
David Agudelo <davidagud@gmail.com>
2018-09-21 22:20:04 -0500

    5.9 validation and pagination

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,6 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
-    can :read, Order, id: user.id
+    can :read, Order, user_id: user.id
   end
 end
