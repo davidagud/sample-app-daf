3b84341d0da12cf4b026156abdec65f90c6cdd29
David Agudelo <davidagud@gmail.com>
2018-09-23 17:42:52 -0500

    5.10 added admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,5 +5,8 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
     can :read, Order, user_id: user.id
+    if user.admin? #
+      can :manage, Comment
+    end
   end
 end
