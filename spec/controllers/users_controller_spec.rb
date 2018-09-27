50e77bf12a4abebb04cea97bdfa6d85286e07c08
David Agudelo <davidagud@gmail.com>
2018-09-26 22:23:44 -0500

    6.3 added controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-decribe UsersController, type: :controller do
+describe UsersController, type: :controller do
   let(:user) { User.create!(email: "test@test.com", password: "testtest") }
+  let(:test_user) { User.create!(email: "test@gmail.com", password: "12345678")}
 
   describe 'GET #show' do
     context 'when a user is logged in' do
@@ -9,14 +10,24 @@ decribe UsersController, type: :controller do
         sign_in user
       end
       it 'loads correct user details' do
-        get :show
+        get :show, params: { id: user.id }
         expect(response).to be_ok
-        expect(assigns(:user)).not_to eq user
+        expect(assigns(:user)).to eq user
       end
     end
 
     context 'when a user is not logged in' do
       it 'redirects to login' do
+        get :show, params: { id: user.id }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'when the wrong user is logged in' do
+      before do
+        sign_in test_user
+      end
+      it 'redirects user to homepage' do
         get :show, params: { id: user.id }
         expect(response).to redirect_to(root_path)
       end
