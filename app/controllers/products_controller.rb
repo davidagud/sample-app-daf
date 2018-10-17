168c1edca16228a80ce34adf2e77d65f95a0798c
David Agudelo <davidagud@gmail.com>
2018-10-16 21:26:35 -0500

    6.15 limited user access to controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /products
   # GET /products.json
@@ -24,7 +25,6 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
-    byebug
     @product = Product.new
   end
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,10 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
     can :read, Order, user_id: user.id
+    can :read, Product
     if user.admin? # admin abilities
       can :manage, Comment
+      can :manage, Product
     end
   end
 end
